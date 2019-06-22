6b033d3a763a920dbb23f765641637aaa695732a
Daniel Williams <mail@daniel-williams.co.uk>
2019-06-22 16:00:14 +0100

    Added updates to the setup file.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from setuptools.command.build_ext import build_ext as _build_ext
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+    
+# see https://stackoverflow.com/a/21621689/1862861 for why this is here
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
+        
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -14,28 +25,35 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    # TODO: put package requirements here
-]
+with open("requirements.txt") as requires_file:
+    requirements = requires_file.read().split("\n")
 
 test_requirements = [
-    # TODO: put package test requirements here
+    "py",
+    "pytest",
+    "coverage"
+]
+
+setup_requirements = [
+    'numpy',
+    'setuptools_scm'
 ]
 
 setup(
     name='gravpy',
-    version='0.1.0',
+    use_scm_version=True,
     description="A sandbox for gravitational wave astronomy.",
     long_description=readme + '\n\n' + history,
     author="Daniel Williams",
-    author_email='d.williams.2@research.gla.ac.uk',
-    url='https://github.com/transientlunatic/gravpy',
+    author_email='daniel.williams@glasgow.ac.uk',
+    url='https://github.com/transientlunatic/grasshopper',
     packages=[
         'gravpy',
     ],
     package_dir={'gravpy':
                  'gravpy'},
     include_package_data=True,
+    setup_requires = setup_requirements,
     install_requires=requirements,
     license="ISCL",
     zip_safe=False,
